30deacfd154048be85d0526855e1d46dcfcc2e74
Adil Shamji <adil02.shamji@gmail.com>
2025-03-14 22:41:20 +0100

    tabout, telescope fix

---
+++ lua/adil/plugins/nvim-autoPairs.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- Equivalent to `require("nvim-autopairs").setup({})`
+    }
+}
+
---
+++ notes.md
@@ -0,0 +1,3 @@
+## Telescope:
+    - Need to have ripgrep, brew.
+    - brew install ripgrep
