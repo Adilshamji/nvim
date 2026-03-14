b109b6a78f39fc0c2ba09e509a6dd66161918108
Adil Shamji <adil02.shamji@gmail.com>
2026-03-14 08:11:59 +0100

    deleated clutter

--- lua/adil/plugins/nvim-wiki.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lervag/wiki.vim",
-  -- tag = "v0.10", -- Uncomment to pin to a specific release
-  config = function()
-    vim.g.wiki_root = "~/Documents/wiki"  -- Set the default wiki directory
-  end
-}
-
